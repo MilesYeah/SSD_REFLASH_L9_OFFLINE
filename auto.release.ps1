72d6e426f41354921a0cb573cc840ff3889ba6a4
Robert <robert.ye@mic.com.tw>
2021-08-09 16:46:20 +0800

    #### version 207: 2021/08/09

--- auto.release.ps1
+++ auto.release.ps1
@@ -2,14 +2,14 @@
 Write-Host 'Plz check any update for FW updating tool, and did you update the version defined in SCRIPT?'
 $null = [Console]::ReadKey('?')
 
-
-# Write-Host 'Press Any Key to continue following auto-matic actions!' -NoNewline
-Write-Host 'Press Any Key to continue following auto-matic actions!'
-$null = [Console]::ReadKey('?')
-
 pandoc.exe .\release_note.md -f markdown -t html -o .\release_note.html
 
-$commit_msg=(cat .\release_note.md | findstr /i "version.20[0-9]\." | select -first 1)
+$commit_msg=(cat .\release_note.md | findstr /i "version.20[0-9]" | select -first 1)
+echo "To be committed version: $commit_msg"
+
+# Write-Host 'Press Any Key to continue following auto-matic actions!' -NoNewline
+Write-Host 'Press Any Key to commit!'
+$null = [Console]::ReadKey('?')
 
 git add .
 git commit -m "$commit_msg"
